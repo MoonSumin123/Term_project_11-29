631defbda1bc9559dfcf00eccb868758c2231bb4
jsejeong <jsejeong4@dgist.ac.kr>
2024-12-01 07:39:20 +0900

    withdraw에 record 넣음

--- Project4/STATEWITHDRAW.h
+++ Project4/STATEWITHDRAW.h
@@ -8,7 +8,7 @@ private:
 	int& withdrawal_count;
 	bool endSession;
 public:
-	state_withdraw(Account& account, ATM& atm, bool primary, Language& lang, int& withdrawal_count, bool& endSession) : State(account, atm, primary), withdrawal_count(withdrawal_count), endSession(endSession){ s_name = "Withdraw State"; };
+	state_withdraw(Account& account, ATM& atm, bool primary, Language& lang, int& withdrawal_count, bool& endSession) : State(account, atm, primary, lang), withdrawal_count(withdrawal_count), endSession(endSession){ s_name = "Withdraw State"; };
 	~state_withdraw() {};
 	void stateAction() override;
 };
--- Project4/stateAction.cpp
+++ Project4/stateAction.cpp
@@ -195,16 +195,22 @@ void state_withdraw::stateAction() {
 	if (avail) {
 		string result = atm.withdraw(amount, withdrawal_fee);
 		account.subFund(amount+withdrawal_fee);
+		
+		string rec_account;
+		string rec_atm;
 
+		rec_account = lang.chooseSentence(17) + account.getCardNumber() + "/" + lang.chooseSentence(19) + to_string(amount) + lang.chooseSentence(21) + "/" + lang.chooseSentence(22) + account.getAccountNumber() + ", " + to_string(account.getFund()) + "/" + lang.chooseSentence(23) + lang.chooseSentence(21) + "- , -" + lang.chooseSentence(21);
+		rec_atm = lang.Eng(17) + account.getCardNumber() + "/" + lang.Eng(19) + to_string(amount) + lang.Eng(21) + "/" + lang.Eng(22) + account.getAccountNumber() + ", " + to_string(account.getFund()) + lang.Eng(21) + "/" + lang.Eng(23) + "- , -" + lang.Eng(21);
+
+		atm.recordRecentHistory(rec_account);
+		atm.recordAtmHistory(rec_atm);
 
 		withdrawal_count++;
 		oss << result;
 	}
 	else 
 		oss << "There is not enough cash in the ATM.";
-	
-	account.recordAccountHistory(oss.str());
-	atm.recordAtmHistory(oss.str());
+
 	cout << oss.str();
 }
 
